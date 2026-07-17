e535662ffe5ea34f8d86494e3288e13705b35bdb
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-18 00:29:50 +0800

    mcp

--- SpringAI/mcp/src/main/java/com/wjl/controller/ChatController.java
+++ SpringAI/mcp/src/main/java/com/wjl/controller/ChatController.java
@@ -19,4 +19,9 @@ public class ChatController {
     public String open(){
         return chatClient.prompt().user("帮我打开浏览器，用百度搜索今日天气").call().content();
     }
+
+    //自定义 MCP
+    //1. 定义一个工具 @Tool 声明式定义或者通过反射编程式定义
+    //2. @Bean 通过 ToolCallBackProvider 交由 Spring 管理
+    //3. 打包成 jar 文件，粗暴的方式直接本地调用 jar 包即可，或者也可以上传远端部署
 }
