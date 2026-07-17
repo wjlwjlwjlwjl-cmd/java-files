9ce38a788c690970254f382b2e79791816398108
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-17 13:20:56 +0800

    springai and springaialibaba quickstart

---
+++ SpringAI(quick start)/spring-ai-alibaba/src/main/java/com/example/spring_ai_alibaba/SpringAiAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai_alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAlibabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiAlibabaApplication.class, args);
+	}
+
+}
--- SpringAI(quick start)/spring-ai-demo/src/main/java/com/example/controller/ChatController.java
+++ SpringAI(quick start)/spring-ai-demo/src/main/java/com/example/controller/ChatController.java
@@ -21,6 +21,7 @@ import reactor.core.publisher.Flux;
 @SuppressWarnings("null")
 public class ChatController {
     private ChatClient chatClient;
+    private ChatClient ollamChatClient;
     private record introduction(String name, List<String> ingredients, String taste){};
     private OpenAiApi openAiApi = new OpenAiApi("https://dashscope.aliyuncs.com/compatible-mode", "sk-a5f277ca38f8426ba23c68a313c6c46b");
     private OpenAiChatModel openAiChatModel = new OpenAiChatModel(openAiApi, new OpenAiChatOptions().builder().model("qwen-plus").build());
@@ -30,6 +31,10 @@ public class ChatController {
             .defaultOptions(OpenAiChatOptions.builder().model("qwen-plus").build())
             .defaultSystem("你是一个厨师，我给你一道菜，介绍一下制作步骤")      //设置系统提示词
             .defaultAdvisors(new SimpleLoggerAdvisor())         //Advisor是介于用户请求和模型之间的组件，对用户提示进行过滤、增强等，思想借鉴了 Spring AOP
+            .build();
+
+        this.ollamChatClient = chatClient
+            .defaultOptions(OpenAiChatOptions.builder().model("deepseek-r1:1.5b").build())
             .build();
     }
 
@@ -65,4 +70,14 @@ public class ChatController {
         Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
         return openAiChatModel.call(prompt).getResult().getOutput().getText();
     }
+
+    @RequestMapping(value = "/stream2", produces = "text/html;charset=utf-8")
+    public Flux<String> stream2(String msg){
+        Flux<ChatResponse> flux = openAiChatModel.stream(new Prompt(msg));
+        return flux.map(s -> s.getResult().getOutput().getText());
+    }
+
+    //ChatModel与ChatClient对比
+    //ChatModel 更偏底层，能够自定义与大模型交互的参数配置与相应细节，灵活性更高
+    //ChatClient 对 ChatModel 进行了封装，自动集成了提示词管理、结构化输出、链式 API、Advisor 功能
 }
