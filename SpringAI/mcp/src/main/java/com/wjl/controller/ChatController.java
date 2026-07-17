79aa35bcd533d0fee0665c812ff8b19e337d2c82
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-07-18 00:23:53 +0800

    mcp

--- SpringAI/mcp/src/main/java/com/wjl/controller/ChatController.java
+++ SpringAI/mcp/src/main/java/com/wjl/controller/ChatController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final ChatClient chatClient;
 
+    //添加mcp工具到ChatClient，同时需要在application.yaml中配置mcp配置
     public ChatController(ChatClient.Builder builder, ToolCallbackProvider toolCallbackProvider){
         this.chatClient = builder.defaultToolCallbacks(toolCallbackProvider).build();
     }
 
-    @RequestMapping("/train")
-    public String train(String msg){
-        return chatClient.prompt().user(msg).call().content();
+    @RequestMapping("/open")
+    public String open(){
+        return chatClient.prompt().user("帮我打开浏览器，用百度搜索今日天气").call().content();
     }
 }
